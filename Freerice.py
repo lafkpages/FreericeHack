1e8d58c98962039aa71f5f20a04571445ab14efe
The Lafkpages Team <mail@lafkpages.tech>
2020-12-17 06:52:33 +0000

    Add files via upload
    Added:
     - exit codes
     - detection for Freerice servers' unavailability
     - if there's an error, create a new game

--- Freerice.py
+++ Freerice.py
@@ -1,18 +1,22 @@
 import requests as r
 
 class Data:
-	def __init__(self):
-		self.game = ''
+  def __init__(self):
+    self.error      = False
+    self.error_info = []
 
-		self.rice_total = 0
+    self.game = ''
+
+    self.rice_total = 0
 		
-		self.question_id  = ''
-		self.question_txt = ''
+    self.question_id  = ''
+    self.question_txt = ''
 
 class Freerice:
   def __init__(self, user_id):
-    self.user = user_id
-    self.game = ''
+    self.user       = user_id
+    self.game       = ''
+    self.init_level = 1
 
     self.new_game_url = 'https://engine.freerice.com/games'
     self.new_game_mth = 'POST'
@@ -33,15 +37,23 @@ class Freerice:
 
     data = {
       'category': self.categories['multiplication-table'],
-      'level': 2,
+      'level': self.init_level,
       'user': self.user
     }
 
     req = r.request(self.new_game_mth, self.new_game_url, json=data, headers=headers)
 
-    data = req.json()
-    
     ret = Data()
+
+    try:
+      data = req.json()
+    except:
+      ret.error = True
+      ret.error_info = 'JSON decode error.'
+
+      return ret
+    
+    
     ret.game         = data['data']['id']
     ret.question_id  = data['data']['attributes']['question_id']
     ret.question_txt = data['data']['attributes']['question']['text']
@@ -66,16 +78,33 @@ class Freerice:
     url = self.answer_url + self.game + self.answer_url2
 	
     req = r.request(self.answer_mth, url, json=data, headers=headers)
-   	
-    data = req.json()
-    
+
     ret = Data()
+
+    try:
+      data = req.json()
+    except:
+      ret.error = True
+      ret.error_info = 'JSON decode error.'
+
+      return ret
+
+    if 'errors' in data:
+      ret.error      = True
+      ret.error_info = data['errors']
+
+      return ret
+    
     try:
       #ret.game         = data['data']['id']
       ret.question_id  = data['data']['attributes']['question_id']
       ret.question_txt = data['data']['attributes']['question']['text']
-      ret.rice_total   = data['data']['attributes']['userattributes']['rice'] #data['data']['attributes']['user_rice_total']
+    except:
+      pass#print(data)
+
+    try:
+      ret.rice_total = data['data']['attributes']['userattributes']['rice'] #data['data']['attributes']['user_rice_total']
     except KeyError:
-      pass
+      ret.rice_total = 0
 
     return ret
--- Requester.py
+++ Requester.py
@@ -5,7 +5,9 @@ from time import sleep
 # CONFIG
 user = '14cd5145-e13c-40fe-aad0-7a2bfb31b2f1'
 log  = False
-secs = 0.005
+secs = False
+fsuv = 6      # exit code for freerice servers unavailable
+ucnc = 0      # exit code for user control C
 
 # Define the hack class with your user id
 # User ID can be found in LocalStorage > user > uuid
@@ -14,10 +16,15 @@ freerice = Freerice(user) # Your user ID
 # Create a new game
 last = freerice.newGame()
 
-print('Initial rice (total): ' + str(last.rice_total))
+if last.error:
+  print('Freerice servers are unavailable. Try changing your IP (via VPN)')
+  quit(fsuv)
 
 try:
   while True:
+    if not (last.rice_total == 0 or last.rice_total == ''):
+      print(str(last.rice_total), end='')
+
     if log:
       print('Game ID:          ' + last.game)
       print('Total rice:       ' + str(last.rice_total))
@@ -25,12 +32,17 @@ try:
       print('Next question:    ' + last.question_txt)
       print('\n')
 
+    if last.error:
+      last = freerice.newGame()
+    
     ans = str(eval(last.question_txt.replace('x', '*')))
 
     last = freerice.submitAnswer(last.question_id, ans)
 
-    sleep(secs)
+    if type(secs) == type(0):
+      sleep(secs)
+
+    print('\r', end='')
 except KeyboardInterrupt:
-  print('User controlled C')
-  print('Final rice (total): ' + str(last.rice_total))
-  quit()
+  print('\r\nUser controlled C')
+  quit(ucnc)
