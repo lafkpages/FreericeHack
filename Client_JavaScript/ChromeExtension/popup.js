03399c4ab17f318c8fed0e8b02dfa45fcc75d885
The Lafkpages Team <mail@lafkpages.tech>
2021-02-05 08:50:32 +0100

    Add files via upload

---
+++ Client_JavaScript/ChromeExtension/popup.js
@@ -0,0 +1,27 @@
+// Define vars representing HTMLDom
+let IntrSct = document.querySelector('#IntrSct');  // <input id="IntrSct" type="range">
+let Intr    = document.querySelector('#Intr');     // <span id="Intr">
+let IntrReq = document.querySelector('#IntrReq');  // <button id="IntrReq">
+let IntrClk = document.querySelector('#IntrClk');  // <button id="IntrClk">
+let SpbrClk = document.querySelector('#SpbrClk');  // <button id="SpbrClk">
+
+// Add event listeners
+IntrSct.addEventListener('oninput', function()
+{
+  console.debug('Moved slider to ' + IntrSct.value);
+  Intr.innerText = IntrSct.value;
+});
+
+IntrClk.addEventListener('click', function()
+{
+  chrome.tabs.getSelected(null, function(tab)
+  {
+    chrome.tabs.executeScript(tab.id, {
+      code: 'window.freeRiceHackIntr = ' + IntrSct.value.toString() + ';'
+    });
+
+    chrome.tabs.executeScript(tab.id, {
+      file: 'IntrClk.js'
+    });
+  });
+});
