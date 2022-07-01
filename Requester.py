b5d24d3df95f524d66d28c9caf4691e8f48fdc20
The Lafkpages Team <62298468+lafkpages@users.noreply.github.com>
2022-07-01 14:14:14 +0000

    Changed default user

--- Requester.py
+++ Requester.py
@@ -35,7 +35,7 @@ import os
 # ============= CONFIG =============
 
 # User
-USER    = ('FREERICE_USER', '5bd2ba4e-ed08-4fc8-8f2f-c56ac0c15f68')
+USER    = ('FREERICE_USER', 'd8c169c0-d076-469a-b3a4-9ad9b532135e')
 user    = os.environ.get(*USER)                    # user ID (can be found in LocalStorage > user > uuid)
 monitor = False
 mntr_gp = False
